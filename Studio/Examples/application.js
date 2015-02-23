d6709e8739c0ee1aeed25a2fd8cf1362704cf244
kumol508 <kumol508@newschool.edu>
2015-02-23 23:10:59 +0000

    test

---
+++ Studio/Examples/application.js
@@ -0,0 +1,11 @@
+$();
+//dollar sign is a function, the whole document is a function under it
+
+//define a variable
+var name = 'Brian';
+
+function say_hi(name){ 
+    alert ('hello ' + name)
+    //string (something in 'quotes' + variable, need space after the o)
+    
+}
