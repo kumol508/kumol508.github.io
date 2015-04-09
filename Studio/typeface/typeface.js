7d947a95d5e1f3e1bc79f246c8b55f1546eb7c58
kumol508 <kumol508@newschool.edu>
2015-04-09 14:43:04 +0000

    our generation update

--- Studio/typeface/typeface.js
+++ Studio/typeface/typeface.js
@@ -40,3 +40,14 @@ $('.usection, .usection2, .usection3, .usection4, .usection5, .usection6, .usect
 
 });
 
+$('.tsection, .tsection2').click(function(event) {
+    var text     =  $(this).children('.text'),
+        target   = $(this).data('target'),
+        offset_y = $(target).offset().top;
+    
+    $(text).addClass('transition');
+    
+    $('html, body').delay(1000).animate({ 'scrollTop': offset_y });
+
+});
+
