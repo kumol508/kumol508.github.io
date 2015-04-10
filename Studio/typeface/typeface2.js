34e0b4096f1d3ac004e798bdc366076762234256
kumol508 <kumol508@newschool.edu>
2015-04-10 02:49:02 +0000

    site

---
+++ Studio/typeface/typeface2.js
@@ -0,0 +1,35 @@
+
+
+$('.section, .section2, .section3, .section4, .section5, .section6, .section7, .section8, .section9, .section10, .section11, .section12').click(function(event) {
+    var text     =  $(this).children('.text'),
+        target   = $(this).data('target'),
+        offset_y = $(target).offset().top;
+    
+    $(text).addClass('transition');
+    
+    $('html, body').delay(1000).animate({ 'scrollTop': offset_y });
+
+});
+
+$('.usection, .usection2, .usection3, .usection4, .usection5, .usection6, .usection7, .usection8, .usection9, .usection10, .usection11, .usection12').click(function(event) {
+    var text     =  $(this).children('.text'),
+        target   = $(this).data('target'),
+        offset_y = $(target).offset().top;
+    
+    $(text).addClass('transition');
+    
+    $('html, body').delay(1000).animate({ 'scrollTop': offset_y });
+
+});
+
+$('.tsection, .tsection2').click(function(event) {
+    var text     =  $(this).children('.text'),
+        target   = $(this).data('target'),
+        offset_y = $(target).offset().top;
+    
+    $(text).addClass('transition');
+    
+    $('html, body').delay(1000).animate({ 'scrollTop': offset_y });
+
+});
+
