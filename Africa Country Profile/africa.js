eb22e095367f411f5795828b90099066581b146d
kumol508 <kumol508@newschool.edu>
2017-11-11 21:33:25 +0000

    presentbrief

---
+++ Africa Country Profile/africa.js
@@ -0,0 +1,29 @@
+$('.content,.button,.friend-button,.graduation-button,.home-button,.wedding-button,.button-end').click(function(event) {
+    var text     =  $(this).children('.text'),
+        target   = $(this).data('target'),
+        offset_y = $(target).offset().top;
+    
+    $(text).addClass('transition');
+    
+    $('html, body').delay(1000).animate({ 'scrollTop': offset_y });
+
+    });
+   
+$('.option1,.option2').click(function(){
+    $('.wrong,.buttontext1').toggle('slow');
+})
+$('.option3,.option4').click(function(){
+    $('.wrong2,.buttontext2').toggle('slow');
+})
+
+$('.option5,.option6').click(function(){
+    $('.wrong3,.buttontext3').toggle('slow');
+})
+
+$('.option7,.option8').click(function(){
+    $('.wrong4,.buttontext4').toggle('slow');
+})
+
+$('.option9,.option10').click(function(){
+    $('.wrong5,.buttontext5').toggle('slow');
+})
