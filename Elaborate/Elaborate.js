e35855db7d5006b47ffd9ff6e0ae2327492dec22
kumol508 <kumol508@newschool.edu>
2018-02-20 00:03:37 +0000

    elab

---
+++ Elaborate/Elaborate.js
@@ -0,0 +1,19 @@
+$('.driver1,.ped1,.neither1').click(function(){
+    $('.clip1code').toggle('slow');
+})
+
+$('.driver2,.ped2,.neither2').click(function(){
+    $('.clip2code').toggle('slow');
+})
+
+$('.driver3,.ped3,.neither3').click(function(){
+    $('.clip3code').toggle('slow');
+})
+
+$('.driver4,.ped4,.neither4').click(function(){
+    $('.clip4code').toggle('slow');
+})
+
+$('.driver5,.ped5,.neither5').click(function(){
+    $('.clip5code').toggle('slow');
+})
